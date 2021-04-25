6ffbb56a920aa98597a56dac9fa79dc903648ffc
Дмитрий Смирнов <painven@gmail.com>
2021-04-26 00:56:28 +0300

    Правки в контрол отображения последних добавленых товаров

--- EtkBlazorApp/Model/ProductViewModel.cs
+++ EtkBlazorApp/Model/ProductViewModel.cs
@@ -8,14 +8,18 @@ namespace EtkBlazorApp
         public int Id { get; set; }
         public string Name { get; set; }
         public string Model { get; set; }
+        public string Manufacturer { get; set; }
         public string Sku { get; set; }
         public string EAN { get; set; }
         public string Uri { get; set; }
+        public string FullSizeImage { get; set; }
         public decimal Price { get; set; }
         public decimal BasePrice { get; set; }
         public string BasePriceCurrency { get; set; }
         public string StockStatus { get; set; }   
         public int Quantity { get; set; }
         public string DateModified { get; set; }
+
+        public string ProductIdUri => $"https://etk-komplekt.ru/index.php?route=product/product&product_id={Id}";
     }
 }
