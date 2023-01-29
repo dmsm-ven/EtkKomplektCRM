18e2dc6621672198d168afeea3492d2f5c1c9edf
Дмитрий Смирнов <painven@gmail.com>
2023-01-29 21:03:03 +0300

    Обновление карусели на главной

---
+++ EtkBlazorApp/Helpers/MapperProfiles/Product/ProductMapperProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using EtkBlazorApp.DataAccess.Entity;
+using System.Web;
+
+namespace EtkBlazorApp.Helpers.MapperProfiles.Product;
+
+public class ProductMapperProfile : Profile
+{
+    public ProductMapperProfile()
+    {
+        CreateMap<ProductEntity, ProductViewModel>()
+            .ForMember(p => p.Image, x => x.MapFrom(p => p.image))
+            .ForMember(p => p.Name, x => x.MapFrom(p => HttpUtility.HtmlDecode(p.name) ?? string.Empty))
+            .ForMember(p => p.Manufacturer, x => x.MapFrom(p => p.manufacturer))
+            .ForMember(p => p.Price, x => x.MapFrom(p => p.price))
+            .ForMember(p => p.Id, x => x.MapFrom(p => p.product_id))
+            .ForMember(p => p.NumberOfViews, x => x.MapFrom(p => p.viewed))
+            .ForMember(p => p.DiscountedPrice, x => x.MapFrom(p => p.discount_price));
+    }
+}
+
