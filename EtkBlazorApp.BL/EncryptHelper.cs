b7ce5e61b0d1e216429641f53e1f5c0c24128929
Дмитрий Смирнов <painven@gmail.com>
2023-02-24 22:24:43 +0300

    enc fix

---
+++ EtkBlazorApp.BL/EncryptHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+using System.Linq;
+
+namespace EtkBlazorApp.BL
+{
+    public static class EncryptHelper
+    {
+        static string MissingField = "email_extractor";
+        static byte[] salt = new byte[] { 0x49, 0x76, 0xaa, 0x6e, 0x20, 0x4d, 0x65, 0x8, 0x76, 0x65, 0x64, 0x32, 0x76 };
+
+        public static string Encrypt(string textToEncrypt)
+        {
+            byte[] clearBytes = Encoding.Unicode.GetBytes(textToEncrypt);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(MissingField, salt);
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    textToEncrypt = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return textToEncrypt;
+        }
+        public static string Decrypt(string encryptedText)
+        {
+            encryptedText = encryptedText.Replace(" ", "+");
+            byte[] cipherBytes = Convert.FromBase64String(encryptedText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(MissingField, salt);
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    encryptedText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return encryptedText;
+        }
+    }
+}
