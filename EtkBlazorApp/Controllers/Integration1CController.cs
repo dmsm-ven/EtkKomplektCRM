bb9f482fa30c8dffc0fa50bed71a717e72751799
Дмитрий Смирнов <painven@gmail.com>
2023-05-03 01:09:34 +0300

    Вынос проверки авторизации при обращении к API от 1C из контроллера в атрибут

--- EtkBlazorApp/Controllers/Integration1CController.cs
+++ EtkBlazorApp/Controllers/Integration1CController.cs
@@ -1,43 +1,26 @@
 ﻿using EtkBlazorApp.Core.Data.Integration1C;
+using EtkBlazorApp.Model.Attributes;
 using EtkBlazorApp.Model.IOptionProfiles;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace EtkBlazorApp.Controllers;
 
 [ApiController]
+[Integration1CAuthorize]
 [Route("api/integration-1c")]
 public class Integration1CController : Controller
 {
-    private readonly Integration1C_Configuration options;
-
-    public Integration1CController(IOptions<Integration1C_Configuration> options)
-    {
-        this.options = options.Value;
-    }
-
     [HttpPost("update-stock")]
     public IActionResult StockUpdated([FromBody] StoreStockData[] newData)
     {
-        if (!HttpContext.Request.Headers.TryGetValue("Authorize", out var auth))
-        {
-            return BadRequest($"Не указан токен интеграции");
-        }
-
-        string senderToken = auth.ToString()
-            .Replace("Token", string.Empty)
-            .Trim();
-
-        bool isAuthorized = senderToken.Equals(options.Token, System.StringComparison.OrdinalIgnoreCase);
-
-        if (!isAuthorized)
-        {
-            return Unauthorized($"Токен указан не верно: {senderToken}");
-        }
-
         decimal totalQuantity = newData[0]
             .Sku
             .Select(sku => decimal.Parse(sku.quantity))
@@ -46,4 +29,5 @@ public class Integration1CController : Controller
         return Ok($"Сумма остатков по всем артикулам: {totalQuantity}");
     }
 }
+
 
