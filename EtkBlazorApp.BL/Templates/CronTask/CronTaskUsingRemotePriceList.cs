5cf0834dcacdd5fc5bd1432adf5b4a22aec4156f
Painven <painven@gmail.com>
2024-09-09 10:53:38 +0300

    Исправление ошибки проверки на null при новом добавленном задании

--- EtkBlazorApp.BL/Templates/CronTask/CronTaskUsingRemotePriceList.cs
+++ EtkBlazorApp.BL/Templates/CronTask/CronTaskUsingRemotePriceList.cs
@@ -34,7 +34,7 @@ namespace EtkBlazorApp.BL.Templates.CronTask
                 var loader = service.remoteTemplateLoaderFactory.GetMethod(templateInfo.remote_uri, templateInfo.remote_uri_method_name, templateGuid);
                 var response = await loader.GetFile();
 
-                if (!forced && response.Bytes.Length == (int)taskInfo.last_exec_file_size)
+                if (!forced && taskInfo?.last_exec_file_size != null && response.Bytes.Length == (int)taskInfo.last_exec_file_size)
                 {
                     throw new CronTaskSkipException();
                 }
